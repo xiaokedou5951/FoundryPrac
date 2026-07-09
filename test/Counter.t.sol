9808304a6f722273cf9937f2df7bd6828f29418d
mackedou <274176171@qq.com>
2026-07-09 16:14:52 +0800

    0.2.3 查看测试中的地址写法

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,15 +1,28 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.30;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+    address public zhangsan;
+    address public lisi;
+    address public wangwu;
+    address public zhaolu;
 
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+
+        zhangsan = vm.addr(1);
+        console.log("zhangsan:", zhangsan);
+        lisi = makeAddr("lisi");
+        console.log("lisi:", lisi);
+        wangwu = address(0x01);
+        console.log("wangwu:", wangwu);
+        zhaolu = address(1);
+        console.log("zhaolu:", zhaolu);
     }
 
     function test_Increment() public {
